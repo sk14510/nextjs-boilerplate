6d91f11d5c600199a592bef00e2192cd5288551c
sk14510 <116840460+sk14510@users.noreply.github.com>
2025-08-22 19:32:42 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  {
+  "version": 4,
+  "pages404": false,
+  "basePath": "",
+  "redirects": [],
+  "rewrites": { "beforeFiles": [], "afterFiles": [], "fallback": [] },
+  "headers": [],
+  "dynamicRoutes": [],
+  "staticRoutes": [
+    {
+      "page": "/",
+      "regex": "^/$",
+      "routeKeys": {}
+    }
+  ],
+  "dataRoutes": []
+}
 };
 
 export default nextConfig;
